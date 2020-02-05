26aa1381d3b9effdf27ec82bfccf730520c6d1a1
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2020-02-05 22:03:08 +0900

    font awesome works

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import '../assets/stylesheets/application.scss';
+import "../assets/stylesheets/application.scss";
 
 const Hello = ({ name }) => {
   return (
@@ -12,7 +12,7 @@ const Hello = ({ name }) => {
   );
 };
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name='World' />, root);
 }
